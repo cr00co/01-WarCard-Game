36e3e38b90f8902b0fe3486fc0d48f44e4515acc
Anton <antonpuh1162@gmail.com>
2026-02-20 01:19:12 +0200

    Initial Commit

---
+++ WarCard-Game/WarCard_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCard_GameApp.swift
+//  WarCard-Game
+//
+//  Created by Anton on 20/2/26.
+//
+
+import SwiftUI
+
+@main
+struct WarCard_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
